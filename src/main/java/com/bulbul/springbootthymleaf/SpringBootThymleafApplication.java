9943a6a949af88bbfcf6e56cb18b48d6908c8977
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2022-01-13 16:41:00 +0600

    first commit

---
+++ src/main/java/com/bulbul/springbootthymleaf/SpringBootThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.bulbul.springbootthymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bulbul/springbootthymleaf/SpringBootThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulbul.springbootthymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
